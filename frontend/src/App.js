fc2bc838bfc9e5c7a795a3d39a2007e82f4250b3
Ameer 610 <ahamza3360@gmail.com>
2023-01-25 23:31:25 +0500

    V3--React-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">Online Store</a>
+      </header>
+      <main>List of Product</main>
+    </div>
+  );
+}
+
+export default App;
