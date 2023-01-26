fc344878b7b99c58e339e36ac9f09e7eac38400b
Ameer 610 <ahamza3360@gmail.com>
2023-01-27 00:39:13 +0500

    feature-product

---
+++ frontend/src/data.js
@@ -0,0 +1,52 @@
+const data = {
+  products:[{
+    name:"Nike Slim Shirts",
+    slug:"nike-slim-shirt",
+    image:"/images/p1.jpg",
+    category:"shirts",
+    noOfReviews:10,
+    description:"high quality product",
+    ratings:"4.3",
+    price:80,
+    stock:20,
+    brand:"Nike"
+  },
+  {
+    name:"Nike Slim pant",
+    slug:"nike-slim-pant",
+    image:"/images/p2.jpg",
+    category:"pants",
+    noOfReviews:23,
+    description:"high quality product",
+    ratings:4.1,
+    price:130,
+    stock:15,
+    brand:"Nike"
+  },
+  {
+    name:"Adidas Slim Shirts",
+    slug:"adidas-slim-shirt",
+    image:"/images/p3.jpg",
+    category:"shirts",
+    noOfReviews:19,
+    description:"high quality product",
+    ratings:4.7,
+    price:90,
+    stock:40,
+    brand:"Puma"
+  },
+  {
+    name:"Adidas Slim pant",
+    slug:"adidas-slim-pant",
+    image:"/images/p4.jpg",
+    category:"pants",
+    noOfReviews:27,
+    description:"high quality product",
+    ratings:"4.1",
+    price:110,
+    stock:36,
+    brand:"Puma"
+  }
+]
+}
+export default data;
