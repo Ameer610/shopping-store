4e10794590edc576fd04593452d697238285cfd5
Ameer 610 <ahamza3360@gmail.com>
2023-01-30 23:24:07 +0500

    add-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
 import {BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import NavBar from "react-bootstrap/NavBar";
+import Container from "react-bootstrap/Container";
+import {LinkContainer} from "react-router-bootstrap";
+
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className="d-flex flex-column site-container">
       <header>
-       <Link to="/">Online Store</Link>
+        <NavBar bg='dark' variant="dark">
+          <Container>
+            <LinkContainer to="/"> 
+            <NavBar.Brand>
+              Online Store
+            </NavBar.Brand>
+            </LinkContainer>
+          </Container>
+        </NavBar>
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/" element={<HomeScreen/>} />
           <Route path="/product/:slug" element={<ProductScreen/>} />
         </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className="text-center">All rights are reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import  {Link } from "react-router-dom";
 import axios from "axios";
 import logger from "use-reducer-logger";
 
+
 const reducer =(state,action) =>{
   switch(action.type)
   {
